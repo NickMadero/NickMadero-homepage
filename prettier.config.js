8437b1c3d5a578cbf14a9345b230f4b7edb2c76a
NickMadero <madero22@students.rowan.edu>
2022-06-12 20:28:52 -0400

    backend handling

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+const options = {
+    arrowParens: "avoid",
+    singleQuote: true,
+    breacketSpacing: true,
+    endOfLine:'lf', // might be a problem later
+    semi : false,
+    tabWidth: 2,
+    trailingComma:'none'
+}
+module.exports = options
